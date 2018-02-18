aa85b4fc44731b121d255411793b9dded03ae7d3
anujinm <anujin.munhbat@gmail.com>
2018-02-17 19:46:03 -0800

    completed the function do_remove_entry function

--- PhoneDirectory/PD_Application.cpp
+++ PhoneDirectory/PD_Application.cpp
@@ -85,13 +85,12 @@ void do_lookup_entry(Phone_Directory& the_directory)
 	}
 }
 
-void do_remove_entry(Phone_Directory& the_directory) // Exercise 1.8: please complete the function do_remove_entry - Ed/Kent
+void do_remove_entry(Phone_Directory& the_directory) // Exercise 1.8: please complete the function do_remove_entry - Ed/Kent //Anujin Munkhbat
 {
 	string name;
 	cout << "Enter name: ";
 	getline(cin, name);
-	// Complete the rest of this function 
-	
+	the_directory.remove_entry(name);
 }
 
 void do_save(Phone_Directory& the_directory)
