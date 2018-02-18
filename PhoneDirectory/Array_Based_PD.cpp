5af05d91e3e3463c82ddd93ee64766177f843651
anujinm <anujin.munhbat@gmail.com>
2018-02-17 19:42:43 -0800

    completed the remove_entry() method

--- PhoneDirectory/Array_Based_PD.cpp
+++ PhoneDirectory/Array_Based_PD.cpp
@@ -113,14 +113,21 @@ void Phone_Directory::save()
 	@return That person's name or an empty string
 	if not in the directory
 	*/
-string Phone_Directory::remove_entry(const string& name) // Exercise 1.7: please complete the remove_entry() method - Ed/Kent
+string Phone_Directory::remove_entry(const string& name) // Exercise 1.7: please complete the remove_entry() method - Ed/Kent // Anujin Munkhbat
 {
+	int index = find(name);
+	std::string number = the_directory[index].get_number();
+	if (index != -1) {
+		for (int i = index; i < size - 1; i++)
+			the_directory[i] = the_directory[i + 1];
+		size -= 1;
+		return number;
+	}
 
-	// Hint: you can use the code below to shift names down in the directory to remove the selected entry specified by "index"
-	// for (int i = index; i < size - 1; i++)
-		// the_directory[i] = the_directory[i + 1];
+	else {
+		return "";
+	}
 
-	return "";
 }
 
 // Private method implementation
